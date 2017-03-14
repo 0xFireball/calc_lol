c11e781b258dad94c1ee0d9e995923303baa1b7d
0xFireball <0xFireball@outlook.com>
2017-03-14 16:54:08 -0700

    stub token class

--- src/lexer/Token.h
+++ src/lexer/Token.h
@@ -4,10 +4,39 @@
 
 #pragma once
 
-enum class TokenKind {
+#include <string>
 
+enum class TokenKind {
+    ARG_SEP,
+
+    // Braces
+
+    ROUND_BRACE,
+    SQUARE_BRACE,
+    CURLY_BRACE,
+    CLOSE_ROUND_BRACE,
+    CLOSE_SQUARE_BRACE,
+    CLOSE_CURLY_BRACE,
+
+    IDENTIFIER,
+    KEYWORD,
+
+    // Literals
+
+    NUMBER_LITERAL,
+    STRING_LITERAL,
+
+    OPERATOR
 };
 
 class Token {
+public:
+    Token(TokenKind kind, std::string content) {
+        this->kind = kind;
+        this->content = content;
+    }
 
+private:
+    std::string content;
+    TokenKind kind;
 };
