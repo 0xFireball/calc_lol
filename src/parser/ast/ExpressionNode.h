9b15c4b045948c65cb3cbe2543ca20d3e812939a
qxzcode <qxzcraft@gmail.com>
2017-03-15 13:02:49 -0700

    Use public inheritance

--- src/parser/ast/ExpressionNode.h
+++ src/parser/ast/ExpressionNode.h
@@ -5,7 +5,7 @@
 #include "AstNode.h"
 #include "../../lexer/Token.h"
 
-class ExpressionNode : AstNode {
+class ExpressionNode : public AstNode {
 public:
     static ExpressionNode create_from_tokens(std::vector<Token> tokens);
 protected:
