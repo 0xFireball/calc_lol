110458b3e79b7af5cdd1184b51be749f40325e33
qxzcode <qxzcraft@gmail.com>
2017-03-14 17:59:37 -0700

    More Parser skeleton

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -3,3 +3,7 @@
 //
 
 #include "Parser.h"
+
+Parser::Parser(std::vector<Token> tokens):tokens(std::move(tokens)) {
+  
+}
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -4,7 +4,14 @@
 
 #pragma once
 
+#include <vector>
+
+#include "../lexer/Token.h"
+
 class Parser {
 public:
+  Parser(std::vector<Token> tokens);
   
+private:
+  std::vector<Token> tokens;
 };
