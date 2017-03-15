a10001909bec4fcb847c21156c7593df04dc1467
0xFireball <0xFireball@outlook.com>
2017-03-15 12:54:26 -0700

    more parser utils

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,7 +1,7 @@
 
 #include "Parser.h"
 
-Parser::Parser(std::vector <Token> tokens) : tokens(std::move(tokens)) {
+Parser::Parser(std::vector<Token> tokens) : tokens(std::move(tokens)) {
 
 }
 
@@ -19,4 +19,18 @@ ProgramNode Parser::parse_to_ast() {
 
 bool Parser::at_program_end() {
     return read_pos >= tokens.size();
+}
+
+std::vector<Token> Parser::read_token_sequence(TokenKind *expectedTokenKinds) {
+    // TODO
+}
+
+Token Parser::take_token() {
+    auto ret = peek_next_token();
+    ++read_pos;
+    return ret;
+}
+
+Token Parser::peek_next_token() {
+    return tokens[read_pos];
 }
--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -8,9 +8,19 @@
 #include "../parser/ast/ProgramNode.h"
 #include "../parser/ast/StatementSequenceNode.h"
 
+
+#pragma once
+
+#include <stdexcept>
+
+class UnexpectedTokenException : public std::logic_error {
+public:
+    UnexpectedTokenException() : std::logic_error("The method or operation is not implemented.") {};
+};
+
 class Parser {
 public:
-    Parser(std::vector <Token> tokens);
+    Parser(std::vector<Token> tokens);
 
     ProgramNode parse_to_ast();
 
@@ -19,8 +29,14 @@ protected:
 
     bool at_program_end();
 
+    Token peek_next_token();
+
+    Token take_token();
+
+    std::vector<Token> read_token_sequence(TokenKind expectedTokenKinds[]);
+
 private:
-    std::vector <Token> tokens;
+    std::vector<Token> tokens;
     int read_pos = 0;
-    std::stack <StatementSequenceNode> scopes;
+    std::stack<StatementSequenceNode> scopes;
 };
