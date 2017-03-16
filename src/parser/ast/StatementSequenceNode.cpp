2ab5be837487bdd213260fb7e6a7a5ef1c4062c9
0xFireball <0xFireball@outlook.com>
2017-03-15 17:44:19 -0700

    use ptr vector

--- src/parser/ast/StatementSequenceNode.cpp
+++ src/parser/ast/StatementSequenceNode.cpp
@@ -1,6 +1,6 @@
 
 #include "StatementSequenceNode.h"
 
-void StatementSequenceNode::append_statement(AstNode& node) {
-    subnodes.push_back(node);
+void StatementSequenceNode::append_statement(AstNode &node) {
+    subnodes.push_back(std::shared_ptr(&node));
 }
--- src/parser/ast/StatementSequenceNode.h
+++ src/parser/ast/StatementSequenceNode.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <vector>
+#include <memory>
 #include <algorithm>
 #include "AstNode.h"
 
@@ -9,16 +10,16 @@ class StatementSequenceNode : public AstNode {
 public:
     StatementSequenceNode() {}
 
-    StatementSequenceNode(std::vector<AstNode> sequence_nodes) : subnodes(std::move(sequence_nodes)) {
+    StatementSequenceNode(std::vector<std::unique_ptr<AstNode>> sequence_nodes) : subnodes(std::move(sequence_nodes)) {
 
     }
 
-    std::vector<AstNode> get_sub_nodes() { return subnodes; }
+    std::vector<std::shared_ptr<AstNode>> get_sub_nodes() { return subnodes; }
 
     void append_statement(AstNode &node);
 
     virtual void emit_code(CodeEmitter &emitter) {}
 
 private:
-    std::vector<AstNode> subnodes;
+    std::vector<std::shared_ptr<AstNode>> subnodes;
 };
