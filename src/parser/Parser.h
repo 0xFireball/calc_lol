d742cae3463bd7f3a2b3d051fee20fe4adae4a22
0xFireball <0xFireball@outlook.com>
2017-03-17 18:58:04 -0700

    store symbol information per-scope

--- src/parser/Parser.h
+++ src/parser/Parser.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <stack>
+#include <map>
 
 #include "../lexer/Token.h"
 #include "../parser/ast/ProgramNode.h"
@@ -25,6 +26,34 @@ public:
     CompilationError(std::string msg) : std::logic_error(msg) {};
 };
 
+enum class SymbolKind {
+    VARIABLE,
+    FUNCTION
+};
+
+struct SymbolInformation {
+    SymbolInformation(int symbol_scope, SymbolKind symbol_kind, std::string symbol_type) : scope(symbol_scope),
+                                                                                           kind(symbol_kind),
+                                                                                           type(symbol_type) {
+
+    }
+
+    int scope;
+    SymbolKind kind;
+    std::string type;
+};
+
+using symbolmap_t = std::map<std::string, std::shared_ptr<SymbolInformation>>;
+
+struct ScopeInformation {
+    ScopeInformation(StatementSequenceNode *statement_sequence) : statements(statement_sequence) {
+
+    }
+
+    StatementSequenceNode *statements;
+    symbolmap_t symbol_map;
+};
+
 class Parser {
 public:
     Parser(std::vector<Token> tokens);
@@ -32,15 +61,19 @@ public:
     std::shared_ptr<ProgramNode> parse_to_ast();
 
 protected:
-    StatementSequenceNode* peek_scope();
+    StatementSequenceNode *peek_scope_statements();
+    ScopeInformation peek_scope_info();
 
     bool at_program_end();
 
     std::vector<Token> read_until_token(TokenKind endKind, bool eatEnd = true);
+
     std::vector<Token> read_until_statement_end();
+
     Token read_expected_token(TokenKind kind);
 
     Token peek_next_token();
+
     Token take_token();
 
     std::vector<Token> read_token_sequence(std::vector<TokenKind> expectedTokenKinds, bool eatEnd = true);
@@ -48,5 +81,5 @@ protected:
 private:
     std::vector<Token> tokens;
     int read_pos = 0;
-    std::stack<StatementSequenceNode*> scopes;
+    std::stack<ScopeInformation> scopes;
 };
